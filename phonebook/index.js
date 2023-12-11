7fbe9bfe1d15e7dce1d4f745958b7eab71dc89c0
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2023-12-11 12:34:28 +0100

    fully functional

--- phonebook/index.js
+++ phonebook/index.js
@@ -5,10 +5,32 @@ const cors = require("cors");
 require("dotenv").config();
 const Person = require("./models/person");
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.use(express.json());
 app.use(express.static("dist"));
 app.use(cors());
+app.use(requestLogger);
 morgan.token("type", (request, response) => {
   return JSON.stringify({
     name: request.body.name,
@@ -20,11 +42,11 @@ morgan.token("type", (request, response) => {
 //  morgan(":method :url :status :res[content-length] :response-time ms - :type")
 //); // note: this is used if we want to use morgan on each route
 
-app.get("/", morgan("tiny"), (request, response) => {
-  response.send(
-    "<h1>Welcome to MySportskit Shop Once again, Home for all your sporting needs!</h1><p>Over 50% discount on selected items</p>"
-  );
-});
+//app.get("/", morgan("tiny"), (request, response) => {
+ // response.send(
+ //   "<h1>Welcome to MySportskit Shop Once again, Home for all your sporting needs!</h1><p>Over 50% discount on selected items</p>"
+//  );
+//});
 
 app.get("/api/persons", morgan("tiny"), (request, response) => {
   Person.find({}).then((person) => {
@@ -39,23 +61,38 @@ app.get("/info", morgan("tiny"), (request, response) => {
   );
 });
 
-app.get("/api/persons/:id", morgan("tiny"), (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+app.get("/api/persons/:id", morgan("tiny"), (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
+app.delete("/api/persons/:id", morgan("tiny"), (request, response,next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
   }
-});
-
-app.delete("/api/persons/:id", morgan("tiny"), (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
-});
-
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+})
 //const newID = () => {
 //  const maxID =
 //    persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
@@ -78,6 +115,9 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const port = process.env.PORT;
 app.listen(port, () => {
